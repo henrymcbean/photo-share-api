09271c22455937b7cb8a22ae3ebf520eb3283c97
henrymcbean <henry_mcbean@hotmail.com>
2021-10-01 10:51:20 +0100

    Creating a GraphQL API

--- index.js
+++ index.js
@@ -1,160 +1,60 @@
-// 1. Require 'apollo-server'
-const { ApolloServer } = require('apollo-server');
-const { GraphQLScalarType } = require('graphql');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { MongoClient } = require('mongodb');
+const { readFileSync } = require('fs');
+const expressPlayground =
+  require('graphql-playground-middleware-express').default;
+const resolvers = require('./resolvers');
 
-const typeDefs = `
-  scalar DateTime
+require('dotenv').config();
+var typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8');
 
-  enum PhotoCategory {
-    SELFIE
-    PORTRAIT
-    ACTION
-    LANDSCAPE
-    GRAPHIC
+async function start() {
+  const app = express();
+  const MONGO_DB = process.env.DB_HOST;
+  let db;
+
+  try {
+    const client = await MongoClient.connect(MONGO_DB, {
+      useNewUrlParser: true,
+    });
+    db = client.db();
+  } catch (error) {
+    console.log(`
+    
+      Mongo DB Host not found!
+      please add DB_HOST environment variable to .env file
+
+      exiting...
+       
+    `);
+    process.exit(1);
   }
 
-  type User {
-    githubLogin: ID!
-    name: String
-    avatar: String
-    postedPhotos: [Photo!]!
-    inPhotos: [Photo!]!
-  }
-  
-  # 1. Add Photo type definition
-  type Photo {
-    id: ID!
-    url: String!
-    name: String!
-    description: String
-    category: PhotoCategory!
-    postedBy: User!
-    taggedUsers: [User!]!
-    created: DateTime!
-  }
-
-  input PostPhotoInput {
-    name: String!
-    category: PhotoCategory=PORTRAIT
-    description: String
-  }
-
-  type Mutation {
-    postPhoto(input: PostPhotoInput!): Photo!
-  }
-  
-  # 2. Return Photo from allPhotos
-  type Query {
-    totalPhotos: Int!
-    allPhotos: [Photo!]!
-  }
-`;
-
-// 1. A variable that we will increment for unique ids
-var _id = 0;
-
-var users = [
-  { githubLogin: 'mHattrup', name: 'Mike Hattrup' },
-  { githubLogin: 'gPlake', name: 'Glen Plake' },
-  { githubLogin: 'sSchmidt', name: 'Scot Schmidt' },
-];
-
-var tags = [
-  { photoID: '1', userID: 'gPlake' },
-  { photoID: '2', userID: 'sSchmidt' },
-  { photoID: '2', userID: 'mHattrup' },
-  { photoID: '2', userID: 'gPlake' },
-];
-
-var photos = [
-  {
-    id: '1',
-    name: 'Dropping the Heart Chute',
-    description: 'The heart chute is one of my favorite chutes',
-    category: 'ACTION',
-    githubUser: 'gPlake',
-    created: '01/01/2020'
-  },
-  {
-    id: '2',
-    name: 'Enjoying the sunshine',
-    category: 'SELFIE',
-    githubUser: 'sSchmidt',
-    created: '01/02/2020'
-  },
-  {
-    id: '3',
-    name: 'Gunbarrel 25',
-    description: '25 laps on gunbarrel today',
-    category: 'LANDSCAPE',
-    githubUser: 'sSchmidt',
-    created: '2018-04-15T19:09:57.308Z'
-  },
-];
-
-const resolvers = {
-  Query: {
-    // 2. Return the length of the photos array
-    totalPhotos: () => photos.length,
-    allPhotos: () => photos,
-  },
-  // 3. Mutation and postPhoto resolver
-  Mutation: {
-    postPhoto(parent, args) {
-      // 2. Create a new photo, and generate an id
-      var newPhoto = {
-        id: _id++,
-        ...args.input,
-      };
-
-      photos.push(newPhoto);
-      return newPhoto;
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req }) => {
+      const githubToken = req.headers.authorization;
+      const currentUser = await db.collection('users').findOne({ githubToken });
+      return { db, currentUser };
     },
-  },
-  Photo: {
-    // trivial resolver
-    url: (parent) => `http://yoursite.com/img/${parent.id}.jpg`,
-    postedBy: (parent) => {
-      return users.find((u) => u.githubLogin === parent.githubUser);
-    },
-    taggedUsers: (parent) =>
-      tags
-        // Returns an array of tags that only contain the current photo
-        .filter((tag) => tag.photoID === parent.id)
-        // Converts the array of tags into an array of userIDs
-        .map((tag) => tag.userID)
-        // Converts array of userIDs into an array of user objects
-        .map((userID) => users.find((u) => u.githubLogin === userID)),
-  },
-  User: {
-    // trivial resolver
-    postedPhotos: (parent) => {
-      return photos.filter((p) => p.githubUser === parent.githubLogin);
-    },
-    inPhotos: (parent) =>
-      tags
-        // Returns an array of tags that only contain the current user
-        .filter((tag) => tag.userID === parent.id)
-        // Converts the array of tags into an array of photoIDs
-        .map((tag) => tag.photoID)
-        // Converts array of photoIDs into an array of photo objects
-        .map((photoID) => photos.find((p) => p.id === photoID)),
-  },
-  DateTime: new GraphQLScalarType({
-    name: 'DateTime',
-    description: 'A valid date time value.',
-    parseValue: (value) => new Date(value),
-    serialize: (value) => new Date(value).toISOString(),
-    parseLiteral: (ast) => ast.value,
-  }),
-};
+  });
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+  server.applyMiddleware({ app });
 
-// 4. Call listen on the server to launch the web server
-server
-  .listen()
-  .then(({ url }) => console.log(`GraphQL Service running on ${url}`));
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
+  app.get('/', (req, res) => {
+    let url = `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=user`;
+    res.end(`<a href="${url}">Sign In with Github</a>`);
+  });
+
+  app.listen({ port: 4000 }, () =>
+    console.log(
+      `GraphQL Server running at http://localhost:4000${server.graphqlPath}`
+    )
+  );
+}
+
+start();
